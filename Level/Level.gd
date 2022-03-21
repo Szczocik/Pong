9f9c8f2f8700e704fb237385d0d2008b08a49e27
Mariusz Popa <popa.mariusz@gmail.com>
2022-03-21 13:17:47 -0400

    dodanie score

---
+++ Level/Level.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var PlayerScore = 0
+var OpponentScore = 0
+
+func _on_Left_body_entered(body):
+	$Ball.position = Vector2(640,360)
+	OpponentScore += 1
+
+
+func _on_Right_body_entered(body):
+	$Ball.position = Vector2(640,360)
+	PlayerScore += 1
+
+func _process(delta):
+	$PlayerScore.text = str(PlayerScore)
+	$OpponentScore.text = str(OpponentScore)
+	
