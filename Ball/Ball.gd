a2c49534d71e3d5cc678c4423be315b22f8ad1af
Mariusz Popa <popa.mariusz@gmail.com>
2022-03-21 11:40:29 -0400

    poprawka w velocity.y

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -6,7 +6,7 @@ var velocity = Vector2.ZERO
 func _ready():
 	randomize()
 	velocity.x = [-1,1][randi() % 2]
-	velocity.y = [-0,8,0,8][randi() % 2]
+	velocity.y = [-0.8,0.8][randi() % 2]
 	
 func _physics_process(delta):
 	var collision_object = move_and_collide(velocity * speed * delta)
