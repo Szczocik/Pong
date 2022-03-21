e1ce048cf48debf1963207ae3fb914d7674d4c35
Mariusz Popa <popa.mariusz@gmail.com>
2022-03-21 07:37:26 -0400

    level scene

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,12 @@
 extends KinematicBody2D
 
+var speed = 400
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y += 1
+	move_and_slide(velocity * speed)
+		
