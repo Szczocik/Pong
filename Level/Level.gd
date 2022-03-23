b9b434323582e43527b7033ac9e70fe52dbd93cc
Mariusz Popa <popa.mariusz@gmail.com>
2022-03-23 08:32:06 -0400

    add Level sound

--- Level/Level.gd
+++ Level/Level.gd
@@ -9,6 +9,7 @@ func _on_Left_body_entered(body):
 	get_tree().call_group('BallGroup','stop_ball')
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
+	$ScoreSound.play()
 	
 
 func _on_Right_body_entered(body):
@@ -17,11 +18,14 @@ func _on_Right_body_entered(body):
 	get_tree().call_group('BallGroup','stop_ball')
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
+	$ScoreSound.play()
+
 
 func _process(delta):
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
 	$CountdownLabel.text = str(int($CountdownTimer.time_left) + 1)
+
 
 func _on_CountdownTimer_timeout():
 	get_tree().call_group('BallGroup','restart_ball')
