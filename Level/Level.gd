dad604e8111db56babafe44fe2ddd919d9fafc76
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-13 06:42:29 -0400

    game_over

--- Level/Level.gd
+++ Level/Level.gd
@@ -6,28 +6,38 @@ var OpponentScore = 0
 func _on_Left_body_entered(body):
 	$Ball.position = Vector2(640,360)
 	OpponentScore += 1
+	if OpponentScore == 5:
+		game_over()
+	get_tree().call_group('BallGroup','stop_ball')
+	$CountdownTimer.start()
+	$CountdownLabel.visible = true
+	$ScoreSound.play()
+
+	
+
+func _on_Right_body_entered(body):
+	$Ball.position = Vector2(640,360)
+	PlayerScore += 1
+	if PlayerScore == 5:
+		game_over()
 	get_tree().call_group('BallGroup','stop_ball')
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
 	$ScoreSound.play()
 	
 
-func _on_Right_body_entered(body):
-	$Ball.position = Vector2(640,360)
-	PlayerScore += 1
-	get_tree().call_group('BallGroup','stop_ball')
-	$CountdownTimer.start()
-	$CountdownLabel.visible = true
-	$ScoreSound.play()
-
-
 func _process(delta):
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
 	$CountdownLabel.text = str(int($CountdownTimer.time_left) + 1)
+	
 
 
 func _on_CountdownTimer_timeout():
 	get_tree().call_group('BallGroup','restart_ball')
 	$CountdownLabel.visible = false
+	
+
+func game_over():
+	get_tree().change_scene("res://Level/GameWin.tscn")
 	
